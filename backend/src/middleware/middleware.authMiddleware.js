5b089107207cec09414cbce8f5c6954258102fc4
vaibhav <vaibhavkumawat21318@gmail.com>
2024-06-29 09:37:04 +0530

    updated middleware

--- backend/src/middleware/middleware.authMiddleware.js
+++ backend/src/middleware/middleware.authMiddleware.js
@@ -1,43 +1,30 @@
-const jwt = require('jsonwebtoken');
-const projectModel = require('../models/project.model');
+import { User } from "../models/user.modal.js";
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
 
-function verifyToken(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Access denied' });
-    try {
-      console.log(process.env.JWT_SECRET);
-      const decoded = jwt.verify(token,  process.env.JWT_SECRET);
-      req.userId = decoded.user._id;
-      next();
-    } catch (error) {
-      console.error(error)
-      res.status(401).json({ error: 'Invalid token' });
-    }
-};
+export const verifyJWT=asyncHandler(async(req,res,next)=>{
+   try {
+     const token=await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+ 
+     if(!token){
+         throw new apiError(401,"Unauthorized access")
+ 
+     }
+     const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+     const user=await User.findById(decodedToken?._id).select(
+         "-password -refreshToken"
+     )
+ 
+     if(!user){
+         throw new apiError(401,"Invalid access token")
+ 
+     }
+     req.user=user  //user ka acces de diya
+     next()                 
+   } catch (error) {
+    
+        throw new apiError(401,error?.message || "Invalid access token")
+   }
 
-async function verifyOwner(req, res, next) {
-  try {
-    const projectId= req.params.projectId;
-    const tokenOwnerId = req.userId;
-    const projectObj = await projectModel.findOne({
-        _id : projectId
-    }, {
-        ownerId: 1
-    });
-
-    if(projectObj?.ownerId != tokenOwnerId) {
-        return res.status(401).send({
-            "result" : "Project Is not Owned by this user"
-        })    
-    }
-    next();
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-}
-
-module.exports = {
-  verifyOwner,
-  verifyToken
-};
+})
